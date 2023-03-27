cf2a5cb6529520f4f6c56afa53239f2c5f360294
RichardRomero <ric.romero@duocuc.cl>
2023-03-27 15:16:00 -0300

    creando variables de entorno

--- AlegriaApp/settings.py
+++ AlegriaApp/settings.py
@@ -11,16 +11,18 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9xk*s(&71w1lfhsbem_oyjg@bs@imd)prw(7@0(^-5*0ma04$k'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
