17e0f4af421d8c9211aa4953bdf308b3e184260b
CorsairRom <ric.romero@duocuc.cl>
2023-04-19 03:20:07 -0400

    first worker in the model

--- ApiArriendosAlegria/admin.py
+++ ApiArriendosAlegria/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from ApiArriendosAlegria.models import Usuario, Banco, TipoCuenta, TipoTrabajador
+from ApiArriendosAlegria.models import Usuario, Banco, TipoCuenta, TipoTrabajador, Trabajador
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Banco)
 admin.site.register(TipoCuenta)
 admin.site.register(TipoTrabajador)
+admin.site.register(Trabajador)
 
