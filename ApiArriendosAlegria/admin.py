2b553314b1db72445d628c1bc236b3321793a59e
CorsairRom <ric.romero@duocuc.cl>
2023-04-19 01:54:38 -0400

    add admin tipo cuentas

--- ApiArriendosAlegria/admin.py
+++ ApiArriendosAlegria/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from ApiArriendosAlegria.models import Usuario
+from ApiArriendosAlegria.models import Usuario, Banco, TipoCuenta
 
 # Register your models here.
 admin.site.register(Usuario)
+admin.site.register(Banco)
+admin.site.register(TipoCuenta)
