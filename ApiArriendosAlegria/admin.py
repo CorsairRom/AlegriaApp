01d03e95efb4f0f283975eeef92a8e1f545772c8
CorsairRom <ric.romero@duocuc.cl>
2023-04-19 03:03:16 -0400

    adding data in tipo trabajador

--- ApiArriendosAlegria/admin.py
+++ ApiArriendosAlegria/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from ApiArriendosAlegria.models import Usuario, Banco, TipoCuenta
+from ApiArriendosAlegria.models import Usuario, Banco, TipoCuenta, TipoTrabajador
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Banco)
 admin.site.register(TipoCuenta)
+admin.site.register(TipoTrabajador)
 
