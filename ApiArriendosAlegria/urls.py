9e83e46b691c94f0ba42ea652538a1ee8df1e46f
CorsairRom <ric.romero@duocuc.cl>
2023-04-19 01:09:18 -0400

    delete a code with problem

--- ApiArriendosAlegria/urls.py
+++ ApiArriendosAlegria/urls.py
@@ -6,5 +6,5 @@ from ApiArriendosAlegria.views import load_data_bank
 urlpatterns = [
     path('usuario/', user_api_view, name='user_api_view'),
     path('usuario/<int:pk>/', user_detail_api_view, name='user_detail_api_view'),
-    path('load_data_bank/', load_data_bank, name='load_data_bank'),
+    
 ]
