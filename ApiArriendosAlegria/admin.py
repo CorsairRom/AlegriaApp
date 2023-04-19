0ffcdcb423c553b2b29f4bc569a71202a71fab47
CorsairRom <ric.romero@duocuc.cl>
2023-04-19 02:05:42 -0400

    problem with relation tipoCuenta

--- ApiArriendosAlegria/admin.py
+++ ApiArriendosAlegria/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from ApiArriendosAlegria.models import Usuario, Banco, TipoCuenta
+from ApiArriendosAlegria.models import Usuario, Banco
 
 # Register your models here.
 admin.site.register(Usuario)
 admin.site.register(Banco)
-admin.site.register(TipoCuenta)
+
