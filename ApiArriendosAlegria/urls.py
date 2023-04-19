3642ef1f75754c2d144162bc7cb1e37d2a13787b
CorsairRom <ric.romero@duocuc.cl>
2023-04-19 01:10:07 -0400

    delete code with problem

--- ApiArriendosAlegria/urls.py
+++ ApiArriendosAlegria/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from ApiArriendosAlegria.api import user_api_view
 from ApiArriendosAlegria.api import user_detail_api_view
-from ApiArriendosAlegria.views import load_data_bank
+
 
 urlpatterns = [
     path('usuario/', user_api_view, name='user_api_view'),
