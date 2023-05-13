dea8d746c3107bfd001cd764f0d9414c90b43a3a
Simonky <simonasb19@gmail.com>
2023-05-13 09:17:05 -0400

    Log in-out refactorizado y error en session

--- ApiArriendosAlegria/views.py
+++ ApiArriendosAlegria/views.py
@@ -64,41 +64,49 @@ class Login(ObtainAuthToken):
     si se repite la petición POST con la sesión ya iniciada.
     """
     def post(self, request, *args, **kwargs):
+
         login_serializer = self.serializer_class(
             data=request.data, context={'request': request})
-        if login_serializer.is_valid():
-            user = login_serializer.validated_data['user']
-            if user.is_active:
-                token, created = Token.objects.get_or_create(user=user)
-                user_serializer = SerializadorUsuario(user)
-                trabajador = Trabajador.objects.get(usuario_id=user.id)
-                trabajador_tipo = trabajador.tipo_trab
-                if created:
-                    return Response({
-                        'token': token.key,
-                        'usuario': user_serializer.data,
-                        'message': 'Ingreso exitoso.'
-                    }, status=status.HTTP_201_CREATED)
-                else:
-                    # Delete user token
-                    token.delete()
-                    # Delete all sessions for user
-                    all_sessions = Session.objects.filter(
-                        expire_date__gte=datetime.now())
-                    if all_sessions.exists():
-                        for session in all_sessions:
-                            session_data = session.get_decoded()
-                            # auth_user_id is the primary key's user on the session
-                            if user.id == int(session_data.get('_auth_user_id')):
-                                session.delete()
-                    return Response({'error': 'Este usuario ya ha ingresado. Se cerrará la sesión activa.'})
-
-            return Response({'error': 'No se puede ingresar con usuario inactivo'},
-                            status=status.HTTP_401_UNAUTHORIZED)
-
-        return Response({'error': 'Usuario o contraseña incorrecta'},
+        
+        if not login_serializer.is_valid():
+            return Response({'error': 'Usuario o contraseña incorrecta'},
                         status=status.HTTP_400_BAD_REQUEST)
 
+        user = login_serializer.validated_data['user']
+        if not user.is_active:
+            return Response({'error': 'No se puede ingresar con usuario inactivo'},
+                        status=status.HTTP_401_UNAUTHORIZED)
+        
+        token, created = Token.objects.get_or_create(user=user)
+        user_serializer = SerializadorUsuario(user)
+
+        if not created:
+            # Delete user token
+            token.delete()
+            # Delete all sessions for user
+            all_sessions = Session.objects.filter(
+                expire_date__gte=datetime.now())
+            
+            if all_sessions.exists():
+                for session in all_sessions:
+                    session_data = session.get_decoded()
+                    # auth_user_id is the primary key's user on the session
+                    if user.id == int(session_data.get('_auth_user_id')):
+                        session.delete()
+
+            return Response({'error': 'Su sessión quedo activa desde la última vez. Por favor ingrese nuevamente.'},
+                            status=status.HTTP_400_BAD_REQUEST
+                            )
+    
+        return Response({
+                    'token': token.key,
+                    'usuario': user_serializer.data,
+                    'message': 'Ingreso exitoso.'
+                }, status=status.HTTP_201_CREATED)
+
+        
+
+        
 
 class Logout(APIView):
     """
@@ -109,28 +117,30 @@ class Logout(APIView):
             token_request = request.GET.get('token')
             token = Token.objects.filter(key=token_request).first()
 
-            if token:
-                user = token.user
-                # Delete all sessions for user
-                all_sessions = Session.objects.filter(
-                    expire_date__gte=datetime.now())
-                if all_sessions.exists():
-                    for session in all_sessions:
-                        session_data = session.get_decoded()
-                        # auth_user_id is the primary key's user on the session
-                        if user.id == int(session_data.get('_auth_user_id')):
-                            session.delete()
-                # Delete user token
-                token.delete()
-
-                token_message = 'Token eliminado.'
-                session_message = 'Sesión exitosamente cerrada.'
-                return Response({'token_message': token_message,
-                                'session_message': session_message},
-                                status=status.HTTP_200_OK)
-
-            return Response({'error': 'No hay usuario con ese token'},
+            if not token:
+                return Response({'error': 'No hay usuario con ese token'},
                             status=status.HTTP_400_BAD_REQUEST)
+            
+            user = token.user
+
+            token.delete()
+            # Delete all sessions for user
+            all_sessions = Session.objects.filter(
+                expire_date__gte=datetime.now())
+            if all_sessions.exists():
+                for session in all_sessions:
+                    session_data = session.get_decoded()
+                    # auth_user_id is the primary key's user on the session
+                    if user.id == int(session_data.get('_auth_user_id')):
+                        session.delete()
+            # Delete user token
+
+            token_message = 'Token eliminado.'
+            session_message = 'Sesión exitosamente cerrada.'
+
+            return Response({'token_message': token_message,
+                            'session_message': session_message},
+                            status=status.HTTP_200_OK)
 
         except:
             return Response({'error': 'No se ha encontrado el token ingresado'},
