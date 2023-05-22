9f9e699b9861fae6a816051b12210ca40d1648c4
Francisco <fr.gonzalezb@duocuc.cl>
2023-05-22 14:54:56 -0400

    Update settings.py

--- AlegriaApp/settings.py
+++ AlegriaApp/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -146,5 +146,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:4200",
+    '*'
 ]
