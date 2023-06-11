543ff8f273a5c1190fac6502dbef8aca5be932ee
CorsairRom <ric.romero@duocuc.cl>
2023-06-10 23:31:14 -0400

    prueba fecha

--- ApiArriendosAlegria/views.py
+++ ApiArriendosAlegria/views.py
@@ -454,10 +454,9 @@ class DashboardViewSet(GenericViewSet):
     
     @action(detail=False, methods=['get'])
     def info(self, request):
-        today = timezone.now()
         scl = tz.gettz('America/Santiago') 
-        print(today.astimezone(tz=scl))
-        print('servidor',  today)
+        today = datetime.now(tz=scl)
+
         try:
             detalle_arriendo = self.get_queryset().filter(fecha_a_pagar__month = today.month , fecha_a_pagar__year = today.year).order_by('fecha_a_pagar')
             propiedades_con_reajuste = detalle_arriendo.filter(toca_reajuste = True).count()
@@ -476,7 +475,7 @@ class DashboardViewSet(GenericViewSet):
                     arrendatarios_nom = detalle.arriendo.arrendatario.get_name()
                     fecha_pago = detalle.fecha_a_pagar
                     
-                    dias_atrazo = today.astimezone().day - fecha_pago.day
+                    dias_atrazo = today.day - fecha_pago.day
                     if dias_atrazo > 0:
                         atrasados = {
                             'propiedad_cod' : propiedad_cod,
