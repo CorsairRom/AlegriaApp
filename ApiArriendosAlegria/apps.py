ebab6bd49bba16036018f3b1f482921dbd2fd75a
Simonky <simonasb19@gmail.com>
2023-06-18 16:56:48 -0400

    Arreglo de signals

--- ApiArriendosAlegria/apps.py
+++ ApiArriendosAlegria/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class ApiarriendosalegriaConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'ApiArriendosAlegria'
+
+    def ready(self) -> None:
+        import ApiArriendosAlegria.signals
